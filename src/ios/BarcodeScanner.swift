0e1bebdc23a59cef373ddeefc61faf0b8fd91cdc
Quinten Adema <quinten@ademagroup.com>
2024-01-29 21:45:45 +0100

    Update BarcodeScanner.swift

--- src/ios/BarcodeScanner.swift
+++ src/ios/BarcodeScanner.swift
@@ -3,16 +3,8 @@ import UIKit
 
 @available(iOS 13.0, *)
 class BarcodeScanner: CDVPlugin, ZXCaptureDelegate {
-	// Properties
-	private var onCallbacks = [String: String]()
 	private var currentCommand: CDVInvokedUrlCommand?
 	private var zxingCapture: ZXCapture?
-	
-	// Initialization
-	@objc func initialize(_ command: CDVInvokedUrlCommand) {
-		self.currentCommand = command
-		return sendPluginResult(true)
-	}
 
 	func sendPluginResult(_ success: Bool, data: Any? = nil, callbackId: String? = nil) {
 		var pluginResult: CDVPluginResult?
@@ -48,15 +40,12 @@ class BarcodeScanner: CDVPlugin, ZXCaptureDelegate {
 	@objc func scan(_ command: CDVInvokedUrlCommand) {
 		self.currentCommand = command
 		
-		// Create a view controller for the modal
 		let modalViewController = UIViewController()
 		modalViewController.modalPresentationStyle = .pageSheet
 		modalViewController.presentationController?.delegate = self
 		
-		// ZXing Capture setup
 		let capture = ZXCapture()
 
-		// Selecting the ultra-wide camera if available, otherwise the main camera
 		let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInUltraWideCamera, .builtInWideAngleCamera], mediaType: .video, position: .back)
 		if let videoCaptureDevice = videoDeviceDiscoverySession.devices.first {
 		    capture.captureDevice = videoCaptureDevice
@@ -66,26 +55,20 @@ class BarcodeScanner: CDVPlugin, ZXCaptureDelegate {
 		capture.layer.frame = modalViewController.view.bounds
 		modalViewController.view.layer.addSublayer(capture.layer)
 
-		// Keep a reference to the ZXing capture
 		self.zxingCapture = capture
 
-		// Set ZXing delegate
 		capture.delegate = self
 
-		// Start ZXing capture
 		capture.start()
 		
-		// Create the overlay
 		let overlayView = UIView(frame: modalViewController.view.frame)
 		overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
 		modalViewController.view.addSubview(overlayView)
 		
-		// Create a transparent square
 		let squareSize = CGSize(width: 200, height: 200)
-		let squareOrigin = CGPoint(x: (overlayView.bounds.width - squareSize.width) / 2, y: (overlayView.bounds.height - squareSize.height) / 2)
-		var transparentSquare = CGRect(origin: squareOrigin, size: squareSize)
-		
-		transparentSquare.origin.y -= (squareSize.height / 2)
+		let squareOrigin = CGPoint(x: (overlayView.bounds.width - squareSize.width) / 2,
+								   y: (overlayView.bounds.height - squareSize.height) / 2)
+		let transparentSquare = CGRect(origin: squareOrigin, size: squareSize)
 		
 		let maskLayer = CAShapeLayer()
 		let path = CGMutablePath()
@@ -95,26 +78,23 @@ class BarcodeScanner: CDVPlugin, ZXCaptureDelegate {
 		maskLayer.fillRule = .evenOdd
 		overlayView.layer.mask = maskLayer
 		
-		// Present the modal
 		self.viewController.present(modalViewController, animated: true, completion: nil)
 	}
 	
-	// ZXing Delegate Method
 	func captureResult(_ capture: ZXCapture!, result: ZXResult!) {
 		if let result = result {
 			let scannedData = result.text
-			
-			// Stop capture and close modal
-			capture.hard_stop()
 			self.viewController.dismiss(animated: true, completion: nil)
 			
 			sendPluginResult(true, data: scannedData)
-		} else {
-			// No barcode detected, stop capture, close modal, and send false result
+			
 			capture.hard_stop()
+		} else {
 			self.viewController.dismiss(animated: true) {
 				self.sendPluginResult(false, data: "No barcode detected")
 			}
+			
+			capture.hard_stop()
 		}
 	}
 	
@@ -129,7 +109,6 @@ class BarcodeScanner: CDVPlugin, ZXCaptureDelegate {
 @available(iOS 13.0, *)
 extension BarcodeScanner: UIAdaptivePresentationControllerDelegate {
 	func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
-		// Called when the modal is dismissed
 		closeModal()
 	}
 }
